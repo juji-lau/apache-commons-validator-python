abe51e05bf82d7b67ce1d593f35efbe536c9a21c
Sanjana679 <sanjana@nandi.net>
2025-02-22 17:38:27 -0500

    started translating files in org.apache.commons.validator
    Signed-off-by: Sanjana679 <sanjana@nandi.net>

---
+++ src/main/ValidatorResult.py
@@ -0,0 +1,146 @@
+"""
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from types import MappingProxyType
+
+class ValidatorResult:
+    """
+    Contains the results of a set of validation rules processed on a JavaBean.
+    """
+
+    # Got rid of _field because it wasn't being used. 
+    class ResultStatus:
+        """
+        Contains the status of a validation.
+        """
+
+        def __init__(self, valid, result):
+            """
+            Constructs a ResultStatus.
+
+            Args:
+                valid (bool): Whether the validator passed.
+                result (object): Value returned by the validator.
+            """
+            self._valid = valid
+            self._result = result
+            self.serializable = True
+            self.cloneable = False
+
+        @property
+        def valid(self):
+            """Whether or not the validation passed."""
+            return self._valid
+
+        @valid.setter
+        def valid(self, valid):
+            self._valid = valid
+
+        @property
+        def result(self):
+            """The result returned by the validator."""
+            return self._result
+
+        @result.setter
+        def result(self, result):
+            self._result = result
+
+    def __init__(self, field):
+        """
+        Constructs a ValidatorResult with the associated field being validated.
+
+        Args:
+            field: The field that was validated.
+        """
+        # self._field = field
+        # Internal dictionary mapping validator names to ResultStatus objects.
+        self._h_action = {}
+        self.serializable = True
+
+    # @property
+    # def field(self):
+    #     """The field that was validated."""
+    #     return self._field
+
+    def add(self, validator_name, result, value=None):
+        """
+        Add the result of a validator action.
+
+        Args:
+            validator_name (str): Name of the validator.
+            result (bool): Whether the validation passed.
+            value (object, optional): Value returned by the validator.
+        """
+        self._h_action[validator_name] = ValidatorResult.ResultStatus(result, value)
+
+    def contains_action(self, validator_name):
+        """
+        Indicates whether a specified validator is in the result.
+
+        Args:
+            validator_name (str): Name of the validator.
+
+        Returns:
+            bool: True if the validator is in the result; False otherwise.
+        """
+        return validator_name in self._h_action
+
+    @property
+    def action_map(self):
+        """
+        Gets an unmodifiable mapping of validator actions.
+
+        Returns:
+            MappingProxyType: A read-only dictionary mapping validator names to ResultStatus objects.
+        """
+        return MappingProxyType(self._h_action)
+
+    # Deprecated
+    # def get_actions(self):
+    #     """
+    #     Gets an iterator of the action names contained in this result.
+
+    #     Returns:
+    #         Iterator[str]: An iterator over the validator action names.
+    #     """
+    #     return iter(self._h_action.keys())
+
+    def get_result(self, validator_name):
+        """
+        Gets the result of a validation.
+
+        Args:
+            validator_name (str): Name of the validator.
+
+        Returns:
+            object: The result returned by the validator, or None if not found.
+        """
+        status: Final[ResultStatus] = self._h_action.get(validator_name)
+        return None if status is None else status.result
+
+    def is_valid(self, validator_name):
+        """
+        Indicates whether a specified validation passed.
+
+        Args:
+            validator_name (str): Name of the validator.
+
+        Returns:
+            bool: True if the validation passed; False otherwise.
+        """
+        status: Final[ResultsStatus] = self._h_action.get(validator_name)
+        return status is not None and status.valid
---
+++ src/main/ValidatorResults.py
@@ -0,0 +1,113 @@
+"""
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+class ValidatorResults:
+    """
+    This class contains the results of a set of validation rules processed
+    on a JavaBean. This is a Python adaptation of the Java version from
+    Apache Commons Validator.
+    """
+
+    def __init__(self):
+        # Internal dictionary mapping a field key (string) to a ValidatorResult.
+        self._h_results = {}
+        self.serializable = False
+        self.cloneable = False
+
+    def add(self, field, validator_name, result, value=None):
+        """
+        Add the result of a validator action.
+
+        Args:
+            field: The field validated. Expected to have a method get_key() or an attribute 'key'.
+            validator_name (str): The name of the validator.
+            result (bool): The result of the validation.
+            value (object, optional): An optional value returned by the validator.
+        """
+        # Retrieve the key from the field.
+        # TODO: double check Field when implementeds
+        key = field.get_key() if hasattr(field, "get_key") else field.key
+        validator_result = self.get_validator_result(key)
+        if validator_result is None:
+            # Assuming ValidatorResult takes the field as an initializer argument.
+            validator_result = ValidatorResult(field)
+            self._h_results[key] = validator_result
+
+        # Add the validation result to the ValidatorResult.
+        validator_result.add(validator_name, result, value)
+
+    def clear(self):
+        """
+        Clear all results recorded by this object.
+        """
+        self._h_results.clear()
+
+    @property
+    def property_names(self):
+        """
+        Get the set of property names for which at least one message has been recorded.
+        
+        Returns:
+            frozenset: An unmodifiable set of property names.
+        """
+        return frozenset(self._h_results.keys())
+
+    def get_result_value_map(self):
+        """
+        Gets a dictionary of any objects returned from validation routines.
+
+        Returns:
+            dict: A dictionary mapping property keys to the corresponding validator result object
+                  (only if the result is not None and not a boolean).
+        """
+        results = {}
+        for property_key, validator_result in self._h_results.items():
+            for action_key in validator_result.get_actions():
+                res = validator_result.get_result(action_key)
+                if res is not None and not isinstance(res, bool):
+                    results[property_key] = res
+        return results
+
+    def get_validator_result(self, key):
+        """
+        Gets the ValidatorResult associated with the given key.
+
+        Args:
+            key (str): The key generated from the Field (often just the field name).
+
+        Returns:
+            ValidatorResult or None: The result associated with the key, if present.
+        """
+        return self._h_results.get(key)
+
+    def is_empty(self):
+        """
+        Determines if there are no messages recorded in this collection.
+
+        Returns:
+            bool: True if there are no results, False otherwise.
+        """
+        return not self._h_results
+
+    def merge(self, other):
+        """
+        Merge another ValidatorResults instance into this one.
+
+        Args:
+            other (ValidatorResults): Another instance whose results will be merged in.
+        """
+        self._h_results.update(other._h_results)
